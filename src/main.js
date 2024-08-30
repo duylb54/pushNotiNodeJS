9474f085919f9f367f2f7cfe8cc6796f8a0fc8cd
duylb54 <duylb54@gmail.com>
2024-08-30 16:42:20 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { throwIfMissing, sendPushNotification } from './utils.js';
 export default async ({ req, res, log, error }) => {
   try {
     throwIfMissing(req.body, ['deviceToken', 'message']);
-    throwIfMissing(req.body.message, ['title', 'body', 'image']);
+    throwIfMissing(req.body.message, ['title', 'body']);
   } catch (err) {
     return res.json({ ok: false, error: err.message }, 400);
   }
@@ -12,8 +12,7 @@ export default async ({ req, res, log, error }) => {
     const response = await sendPushNotification({
       notification: {
         title: req.body.message.title,
-        body: req.body.message.body,
-        image: req.body.message.image
+        body: req.body.message.body
       },
       // extra options payload here
       data: req.body.data ?? {},
